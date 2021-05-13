a932a8da260ee1450a4d37aa811cfc81f87c4aef
MintPeanut <63995863+MintPeanut@users.noreply.github.com>
2021-05-13 15:46:24 +0900

    js/css 파일 추가

---
+++ GroupEroom/src/main/webapp/resources/js/login.js
@@ -0,0 +1,20 @@
+const id_Login_Box = document.querySelector('.id_Out_Box');
+const password_Login_Box =  document.querySelector('.password_Out_Box');
+const login_Id_Input_Box = document.querySelector('#Id');
+const login_Password_Input_Box = document.querySelector('#Password');
+
+login_Id_Input_Box.addEventListener('keyup', function(){
+    if(!login_Id_Input_Box.value == ''){
+        id_Login_Box.classList.add('existence');  
+    }else{  
+        id_Login_Box.classList.remove('existence');  
+    }
+});
+
+login_Password_Input_Box.addEventListener('keyup', function(){
+    if(!login_Password_Input_Box.value == ''){
+        password_Login_Box.classList.add('existence');  
+    }else{  
+        password_Login_Box.classList.remove('existence');  
+    }
+});
