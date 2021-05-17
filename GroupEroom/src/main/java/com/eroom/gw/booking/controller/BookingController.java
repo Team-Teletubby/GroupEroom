69c35e3fc4764cb76494aac782a7e0c1964ccfff
basakxhr <johello62@gmail.com>
2021-05-18 01:26:32 +0900

    자원관리 수정 중

--- GroupEroom/src/main/java/com/eroom/gw/booking/controller/BookingController.java
+++ GroupEroom/src/main/java/com/eroom/gw/booking/controller/BookingController.java
@@ -1,5 +1,57 @@
 package com.eroom.gw.booking.controller;
 
-public class BookingController {
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.eroom.gw.booking.domain.Booking;
+import com.eroom.gw.booking.service.BookingService;
+
+@Controller
+public class BookingController {
+	
+	@Autowired
+	private MemberService mService;
+	@Autowired
+	private BookingService bService;
+		
+//예약리스트 조회화면
+	@RequestMapping("bkListView.do")
+	public ModelAndView bookingListView (ModelAndView mv, HttpSession session) {
+		
+		return mv;
+	}
+	
+//예약생성 조회화면
+	@RequestMapping("bkInsertView.do")
+	public ModelAndView bookingInsertView(ModelAndView mv) {
+		
+		return mv;
+	}
+	
+//예약생성
+	@ResponseBody
+	@RequestMapping("bkRegster.do")
+	public ModelAndView bookingRegister(HttpSession session, @ModelAttribute Booking booking) {
+		
+	}
+
+//날짜로부터 예약 내역 받아오기
+	@RequestMapping("bkSelectDate.do")
+	public void bookingSelectDate(HttpServletResponse response, 
+								@RequestParam("date") String date, @RequestParam("roomNo") int roomNo) {
+		
+	}
+	
+//예약내용 수정
+	@ResponseBody
+	@RequestMapping("bkUpdate.do")
+	public String bookingUpdate(@RequestParam("id") int id, @RequestParam(""))
 }
--- GroupEroom/src/main/java/com/eroom/gw/booking/domain/Booking.java
+++ GroupEroom/src/main/java/com/eroom/gw/booking/domain/Booking.java
@@ -1,5 +1,91 @@
 package com.eroom.gw.booking.domain;
 
-public class Booking {
+import java.sql.Date;
 
+public class Booking {
+	
+	private int bookingNo;
+	private int roomNo;
+	private int memberId;
+	private String participants;
+	private Date startTime;
+	private Date endTime;
+	private String Contents;
+	private String roomState;
+	
+	public Booking() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Booking(int bookingNo, int roomNo, int memberId, String participants, Date startTime, Date endTime,
+			String contents, String roomState) {
+		super();
+		this.bookingNo = bookingNo;
+		this.roomNo = roomNo;
+		this.memberId = memberId;
+		this.participants = participants;
+		this.startTime = startTime;
+		this.endTime = endTime;
+		Contents = contents;
+		this.roomState = roomState;
+	}
+	
+	public int getBookingNo() {
+		return bookingNo;
+	}
+	public void setBookingNo(int bookingNo) {
+		this.bookingNo = bookingNo;
+	}
+	public int getRoomNo() {
+		return roomNo;
+	}
+	public void setRoomNo(int roomNo) {
+		this.roomNo = roomNo;
+	}
+	public int getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(int memberId) {
+		this.memberId = memberId;
+	}
+	public String getParticipants() {
+		return participants;
+	}
+	public void setParticipants(String participants) {
+		this.participants = participants;
+	}
+	public Date getStartTime() {
+		return startTime;
+	}
+	public void setStartTime(Date startTime) {
+		this.startTime = startTime;
+	}
+	public Date getEndTime() {
+		return endTime;
+	}
+	public void setEndTime(Date endTime) {
+		this.endTime = endTime;
+	}
+	public String getContents() {
+		return Contents;
+	}
+	public void setContents(String contents) {
+		Contents = contents;
+	}
+	public String getRoomState() {
+		return roomState;
+	}
+	public void setRoomState(String roomState) {
+		this.roomState = roomState;
+	}
+	
+	@Override
+	public String toString() {
+		return "Booking [bookingNo=" + bookingNo + ", roomNo=" + roomNo + ", memberId=" + memberId + ", participants="
+				+ participants + ", startTime=" + startTime + ", endTime=" + endTime + ", Contents=" + Contents
+				+ ", roomState=" + roomState + "]";
+	}
+
+	
 }
