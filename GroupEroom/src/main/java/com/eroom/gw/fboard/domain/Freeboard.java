f6305b76c501894a460a5b9d12bdf4ada879d6e7
basakxhr <johello62@gmail.com>
2021-05-18 00:16:37 +0900

    자유게시판 Class Diagram 생성

--- GroupEroom/src/main/java/com/eroom/gw/fboard/domain/Freeboard.java
+++ GroupEroom/src/main/java/com/eroom/gw/fboard/domain/Freeboard.java
@@ -10,5 +10,65 @@ public class Freeboard {
 	private String fBoardContents;
 	private Date enrollDate;
 	private int hits;
+	
+	public Freeboard() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Freeboard(int fBoardNo, int memberId, String fBoardTilte, String fBoardContents, Date enrollDate, int hits) {
+		super();
+		this.fBoardNo = fBoardNo;
+		this.memberId = memberId;
+		this.fBoardTilte = fBoardTilte;
+		this.fBoardContents = fBoardContents;
+		this.enrollDate = enrollDate;
+		this.hits = hits;
+	}
+	
+	public int getfBoardNo() {
+		return fBoardNo;
+	}
+	public void setfBoardNo(int fBoardNo) {
+		this.fBoardNo = fBoardNo;
+	}
+	public int getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(int memberId) {
+		this.memberId = memberId;
+	}
+	public String getfBoardTilte() {
+		return fBoardTilte;
+	}
+	public void setfBoardTilte(String fBoardTilte) {
+		this.fBoardTilte = fBoardTilte;
+	}
+	public String getfBoardContents() {
+		return fBoardContents;
+	}
+	public void setfBoardContents(String fBoardContents) {
+		this.fBoardContents = fBoardContents;
+	}
+	public Date getEnrollDate() {
+		return enrollDate;
+	}
+	public void setEnrollDate(Date enrollDate) {
+		this.enrollDate = enrollDate;
+	}
+	public int getHits() {
+		return hits;
+	}
+	public void setHits(int hits) {
+		this.hits = hits;
+	}
+	
+	@Override
+	public String toString() {
+		return "Freeboard [fBoardNo=" + fBoardNo + ", memberId=" + memberId + ", fBoardTilte=" + fBoardTilte
+				+ ", fBoardContents=" + fBoardContents + ", enrollDate=" + enrollDate + ", hits=" + hits + "]";
+	}
+	
+	
 
 }
--- GroupEroom/src/main/java/com/eroom/gw/fboard/store/logic/FBoardStoreLogic.java
+++ GroupEroom/src/main/java/com/eroom/gw/fboard/store/logic/FBoardStoreLogic.java
@@ -1,5 +1,86 @@
 package com.eroom.gw.fboard.store.logic;
 
-public class FBoardStoreLogic {
+import java.util.ArrayList;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.eroom.gw.common.PageInfo;
+import com.eroom.gw.fboard.domain.Freeboard;
+import com.eroom.gw.fboard.domain.FreeboardCmt;
+import com.eroom.gw.fboard.store.FBoardStore;
+
+@Repository
+public class FBoardStoreLogic implements FBoardStore {
+	
+	@Autowired
+	private SqlSession sqlSession;
+
+	@Override
+	public int selectListHits() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int addHits(int fBoardNo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public ArrayList<Freeboard> selectAllList(PageInfo pi) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Freeboard selectOne(int fBoardNo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int insertFBoard(Freeboard fBoard) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int updateFBoard(Freeboard fBoard) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteFBoard(int fBoardNo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public ArrayList<FreeboardCmt> selectAllCmt(int fBoardNo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int insertFBoardCmt(FreeboardCmt fBoardCmt) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int updateFBoardCmt(FreeboardCmt fBoardCmt) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteFBoardCmt(FreeboardCmt fBoardCmt) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
