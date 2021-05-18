a440a55b3ed4eb753353a0f4f3d8b83b29c5b33c
Kdj <2020ytm@naver.com>
2021-05-18 22:26:37 +0900

    멤버컨트롤

--- GroupEroom/src/main/java/com/eroom/gw/member/controller/MemberController.java
+++ GroupEroom/src/main/java/com/eroom/gw/member/controller/MemberController.java
@@ -2,17 +2,12 @@ package com.eroom.gw.member.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-<<<<<<< HEAD
 
-@Controller
-public class MemberController {
-=======
+
+
+
 import org.springframework.web.bind.annotation.RequestMethod;
->>>>>>> branch 'master' of https://github.com/Team-Teletubby/GroupEroom.git
 
-<<<<<<< HEAD
-
-=======
 @Controller
 public class MemberController {
 	
@@ -22,6 +17,6 @@ public class MemberController {
 		return "index";
 		
 	}
->>>>>>> branch 'master' of https://github.com/Team-Teletubby/GroupEroom.git
+
 	
 }
