9437870e82380f540cc7cb3a2e9730923e425268
Kdj <2020ytm@naver.com>
2021-05-18 15:43:37 +0900

    멤버푸시

--- GroupEroom/src/main/java/com/eroom/gw/member/controller/MemberController.java
+++ GroupEroom/src/main/java/com/eroom/gw/member/controller/MemberController.java
@@ -1,5 +1,11 @@
 package com.eroom.gw.member.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class MemberController {
 
+
+	
 }
