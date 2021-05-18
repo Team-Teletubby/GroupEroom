60421b0deef3995b658f5e9f20fbea6d7681535e
Jeongseo-Han <emotion2021@naver.com>
2021-05-18 15:48:44 +0900

    resources/mappers 폴더생성

--- GroupEroom/src/main/java/com/eroom/gw/attendance/store/logic/AttendanceStoreLogic.java
+++ GroupEroom/src/main/java/com/eroom/gw/attendance/store/logic/AttendanceStoreLogic.java
@@ -2,11 +2,19 @@ package com.eroom.gw.attendance.store.logic;
 
 import java.util.ArrayList;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
 import com.eroom.gw.attendance.domain.Attendance;
 import com.eroom.gw.attendance.store.AttendanceStore;
 
+@Repository
 public class AttendanceStoreLogic implements AttendanceStore{
 
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
 	@Override
 	public ArrayList<Attendance> selectAllAttendance(int memberId) {
 		// TODO Auto-generated method stub
